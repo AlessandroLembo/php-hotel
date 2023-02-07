ab630995c74ed4a280c3e626f4983c94f5c57bfd
AlessandroLembo <alessandrolembo2@gmail.com>
2023-02-07 15:33:06 +0100

    loop for to print hotel's name on page

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+     
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +56,16 @@
 
 </head>
 <body>
-    <p class="text-success">connect</p>
+    <div class="container mt-5">
+    <ul>
+       <?php for ($i = 0; $i < count($hotels); $i++) : ?>
+          <li>
+             <p><?= $hotels[$i]['name'] ?></p>
+          </li>
+        <?php endfor; ?>
+    </ul>
+
+    </div>
+    
 </body>
 </html>
