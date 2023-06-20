75c7568115cb79fea6101857cb140a3cd92985fc
AlessandroLembo <alessandrolembo2@gmail.com>
2023-06-20 19:30:23 +0200

    new array with new value and import font awesome

--- index.php
+++ index.php
@@ -1,50 +1,71 @@
 <?php
 
-    $hotels = [
+$hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
 
-    ];
+];
 
-     
+// dichiaro un array vuoto
+$updateHotels = [];
+
+// giro sull'array di hotel
+foreach ($hotels as $hotel) {
+
+    // In base alla disponibilità di parcheggio uso il metodo array_replace per cambiare la proprietà 'parking' del singolo hotel
+    if ($hotel['parking'] == true) {
+
+        $replacements = array('parking' => '<i class="fa-solid fa-circle-check" style="color: #269c3d;"></i>');
+        $hotel = array_replace($hotel, $replacements);
+    } else {
+
+        $replacements = array('parking' => '<i class="fa-solid fa-circle-xmark" style="color: #d90d0d;"></i>');
+        $hotel = array_replace($hotel, $replacements);
+    }
+
+    // inserisco nell'array gli hotel con la proprità 'parking' aggiornata
+    array_push($updateHotels, $hotel);
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -54,43 +75,49 @@
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 
 <body>
     <div class="container mt-5">
 
-         <table class="table">
-           <thead>
-               <th scope="row" class="mb-5 fs-2">HOTELS</th>
-               <tr>
-                 <th scope="col">#</th>
+        <table class="table">
+            <thead>
+                <th scope="row" class="mb-5 fs-2">HOTELS</th>
+                <tr>
+                    <th scope="col">#</th>
 
-                 <!-- loop for each to print keys -->
-                 <?php foreach ($hotels[0] as $key => $value ) : ?>
-                    <th scope="col"><?= $key ?></th>
-                 <?php endforeach; ?>
-               </tr>
-           </thead>
-        
-         <!-- loop for to print 5 rows -->
-         <tbody>
-           <?php for ($i = 0; $i < count($hotels); $i++) : ?>
-           
-             <tr>
-               <th scope="row"><?= $i + 1 ?></th>
+                    <!-- loop for each to print keys -->
+                    <?php foreach ($hotels[0] as $key => $value) : ?>
+                        <th scope="col"><?= $key ?></th>
+                    <?php endforeach; ?>
+                </tr>
+            </thead>
 
-               <!-- loop for to print value -->
-               <?php foreach ($hotels[$i] as $key => $value) : ?>
-                   <td><?= $value ?></td>
-               <?php endforeach; ?>  
-             </tr>
-           
-           <?php endfor; ?>
-        </tbody>
+            <!-- loop for to print 5 rows -->
+            <tbody>
+                <?php for ($i = 0; $i < count($hotels); $i++) : ?>
 
-         </table>
+                    <tr>
+                        <th scope="row"><?= $i + 1 ?></th>
+
+                        <!-- loop for to print value -->
+                        <?php foreach ($updateHotels[$i] as $key => $value) : ?>
+                            <td><?= $value ?></td>
+                        <?php endforeach; ?>
+                    </tr>
+
+                <?php endfor; ?>
+            </tbody>
+
+        </table>
     </div>
-    
+
 </body>
+
 </html>
