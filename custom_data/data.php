32b320f2a4f183594892aa53156eb3df7a2bb172
AlessandroLembo <alessandrolembo2@gmail.com>
2023-07-27 14:07:49 +0200

    new file data

---
+++ custom_data/data.php
@@ -0,0 +1,21 @@
+<?php
+
+// array sul quale girare per mostrare il voto dell'hotel sottoforma di stelline
+$stars = [
+    [
+        'star' => 1,
+    ],
+    [
+        'star' => 2,
+    ],
+    [
+        'star' => 3,
+    ],
+    [
+        'star' => 4,
+    ],
+    [
+        'star' => 5,
+    ],
+
+];
