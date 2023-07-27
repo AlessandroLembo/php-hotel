8fd369beb964af2a22672d15745cd67c20271c38
AlessandroLembo <alessandrolembo2@gmail.com>
2023-07-27 12:47:10 +0200

    create file and move logic

---
+++ custom_data/logic.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once __DIR__ . '/../data.php';
+
+
+// array vuoto che riempio con le votazioni di ogni hotel
+$arrayStars = [];
+
+// dichiaro un array vuoto per poi riempirlo aggiornato al valore della proprietà 'parking'
+$updateHotels = [];
+
+// giro sull'array di hotel
+foreach ($hotels as $hotel) {
+
+    // giro sull'array stars e controllo quante stelle 'piene' e quante 'vuote' pushare nell'array dichiarato vuoto in precedenza
+    foreach ($stars as $star) {
+
+        // ternario per controllare quante e quali stelle inserie nell'array
+        $hotel['vote'] >= $star['star'] ? array_push($arrayStars, '<i class="fa-solid fa-star" style="color: #c1ad49;"></i>') : array_push($arrayStars, '<i class="fa-regular fa-star" style="color: #c1ad49;"></i>');
+    }
+
+    // converto l'array in stringa
+    $starString = implode($arrayStars);
+
+    // salvo in una variabile il valore nuovo assegnato alla proprietà 'vote'
+    $starsVote = array('vote' => $starString);
+
+    // sostituisco a tutti gli hotels il booleano del voto con la stringa di stelle
+    $hotel = array_replace($hotel, $starsVote);
+
+
+
+    // aggiungo una stringa al voto
+    $hotel['distance'] .= ' Km';
+
+    // In base alla disponibilità di parcheggio uso il metodo array_replace per cambiare la proprietà 'parking' del singolo hotel
+    $hotel['parking'] == true ? $replacements = array('parking' => '<i class="fa-solid fa-circle-check" style="color: #269c3d;"></i>') : $replacements = array('parking' => '<i class="fa-solid fa-circle-xmark" style="color: #d90d0d;"></i>');
+    $hotel = array_replace($hotel, $replacements);
+
+    // inserisco nell'array gli hotel con la proprità 'parking' aggiornata
+    array_push($updateHotels, $hotel);
+
+    // svuoto l'array per riempirlo di stelle del prossimo hotel su cui giro
+    $arrayStars = [];
+}
